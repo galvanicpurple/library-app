018777e4eb16a7560ce575ad46ed47bbd626c9e2
galvanicpurple <isaacchai@hotmail.com>
2026-07-29 15:58:50 +0800

    Add test endpoints for debugging scan and upload issues

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import shelvesRoutes from './routes/shelves.js';
 import readingsRoutes from './routes/readings.js';
 import scanRoutes from './routes/scan.js';
 import recommendationsRoutes from './routes/recommendations.js';
+import testRoutes from './routes/test.js';
 
 // Load environment variables
 dotenv.config();
@@ -44,6 +45,7 @@ app.use('/api/shelves', shelvesRoutes);
 app.use('/api/readings', readingsRoutes);
 app.use('/api/scan', scanRoutes);
 app.use('/api/recommendations', recommendationsRoutes);
+app.use('/api/test', testRoutes);
 
 // 404 handler
 app.use((req, res) => {
